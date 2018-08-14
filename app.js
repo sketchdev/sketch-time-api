3c1054fa3ee179514e192492c346e57b72d840c0
Heather Davis <heather@sketchdev.io>
2018-08-14 14:14:17 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const express = require('express');
+const logger = require('morgan');
+const wristrest = require('wristrest');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.set('trust proxy', 1); // trust first proxy
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(wristrest());
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
